a155a49cd5992b45dc3e632bb1e87a6122fadf9f
Elijah Enoch Egga <eljenochegga@gmail.com>
2022-12-14 07:43:46 -0800

    More Tasks

---
+++ freeall.c
@@ -0,0 +1,20 @@
+#include "monty.h"
+
+/**
+* freeall - free list and global
+* @stack: stack_t
+* Return: void
+*/
+
+void freeall(stack_t **stack)
+{
+stack_t *temp = *stack;
+
+while (temp)
+{
+(*stack) = (*stack)->next;
+free(temp);
+temp = *stack;
+}
+fclose(global.file);
+}
